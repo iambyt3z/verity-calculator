f7ce096f6f7c21bdaaf6e461cf686fa2483ec0b1
Siddhant Thakare <siddhantthakare7@gmail.com>
2024-08-07 16:53:42 +0530

    Added validations for request params

--- api/api.go
+++ api/api.go
@@ -1,19 +1,111 @@
 package api
 
-// Solve Verity Outside Dissection Request Body
-type SolveVerityOutsideDissectionRequestBody struct {
-	InsideRoomLeftStatueSymbol   string
-	InsideRoomMidStatueSymbol    string
-	InsideRoomRightStatueSymbol  string
-	OutsideRoomLeftStatueSymbol  string
-	OutsideRoomMidStatueSymbol   string
-	OutsideRoomRightStatueSymbol string
+import (
+	"errors"
+)
+
+type Shape2d string
+type Shape3d string
+
+const (
+	Circle   Shape2d = "Circle"
+	Square   Shape2d = "Square"
+	Triangle Shape2d = "Triangle"
+
+	Cone     Shape3d = "Cone"
+	Cylinder Shape3d = "Cylinder"
+	Cube     Shape3d = "Cube"
+	Prism    Shape3d = "Prism"
+	Pyramid  Shape3d = "Pyramid"
+	Sphere   Shape3d = "Sphere"
+)
+
+func (s2d Shape2d) Validate() error {
+	switch s2d {
+	case Circle, Square, Triangle:
+		return nil
+	}
+
+	return errors.New("can have only Circle, Square, or Triangle as it's value")
+}
+
+func (s3d Shape3d) Validate() error {
+	switch s3d {
+	case Cone, Cylinder, Cube, Prism, Pyramid, Sphere:
+		return nil
+	}
+
+	return errors.New("can have only Cone, Cylinder, Cube, Prism, Pyramid, or Sphere as it's value")
+}
+
+type SolveVerityRequestBody struct {
+	InsideRoomLeftStatueSymbol   Shape2d
+	InsideRoomMidStatueSymbol    Shape2d
+	InsideRoomRightStatueSymbol  Shape2d
+	OutsideRoomLeftStatueSymbol  Shape3d
+	OutsideRoomMidStatueSymbol   Shape3d
+	OutsideRoomRightStatueSymbol Shape3d
+	IsChallengePhaseTwo          bool
+}
+
+func (req SolveVerityRequestBody) Validate() error {
+	// Empty Value Validations
+	if req.InsideRoomLeftStatueSymbol == "" {
+		return errors.New("InsideRoomLeftStatueSymbol field is not present")
+	}
+
+	if req.InsideRoomMidStatueSymbol == "" {
+		return errors.New("InsideRoomMidStatueSymbol field is not present")
+	}
+
+	if req.InsideRoomRightStatueSymbol == "" {
+		return errors.New("InsideRoomRightStatueSymbol field is not present")
+	}
+
+	if req.OutsideRoomLeftStatueSymbol == "" {
+		return errors.New("OutsideRoomLeftStatueSymbol field is not present")
+	}
+
+	if req.OutsideRoomMidStatueSymbol == "" {
+		return errors.New("OutsideRoomMidStatueSymbol field is not present")
+	}
+
+	if req.OutsideRoomRightStatueSymbol == "" {
+		return errors.New("OutsideRoomRightStatueSymbol field is not present")
+	}
+
+	// Wrong value validations
+	if err := req.InsideRoomLeftStatueSymbol.Validate(); err != nil {
+		return errors.New("InsideRoomLeftStatueSymbol field " + err.Error())
+	}
+
+	if err := req.InsideRoomMidStatueSymbol.Validate(); err != nil {
+		return errors.New("InsideRoomMidStatueSymbol field " + err.Error())
+	}
+
+	if err := req.InsideRoomRightStatueSymbol.Validate(); err != nil {
+		return errors.New("InsideRoomRightStatueSymbol field " + err.Error())
+	}
+
+	if err := req.OutsideRoomLeftStatueSymbol.Validate(); err != nil {
+		return errors.New("OutsideRoomLeftStatueSymbol field " + err.Error())
+	}
+
+	if err := req.OutsideRoomMidStatueSymbol.Validate(); err != nil {
+		return errors.New("OutsideRoomMidStatueSymbol field " + err.Error())
+	}
+
+	if err := req.OutsideRoomRightStatueSymbol.Validate(); err != nil {
+		return errors.New("OutsideRoomRightStatueSymbol field " + err.Error())
+	}
+
+	return nil
 }
 
 // Solve Verity Outside Dissection Response
-type SolveVerityOutsideDissectionResponse struct {
-	Code                   uint
+type SolveVerityResponse struct {
 	OutsideDissectionSteps []string
+	InsideDissectionSteps  [][]string
 }
 
 type Error struct {
--- internal/handlers/api.go
+++ internal/handlers/api.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/iambyt3z/verity-calculator/api"
 	"github.com/iambyt3z/verity-calculator/internal/middleware"
 )
 
@@ -12,9 +15,34 @@ func Handler(app *fiber.App) {
 	app.Use(recover.New())
 	app.Use(middleware.Authorization)
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Status(fiber.StatusOK).JSON(fiber.Map{
-			"message": "Hello World!",
-		})
+	app.Post("/solve-verity", func(c *fiber.Ctx) error {
+		var params = api.SolveVerityRequestBody{}
+
+		err_parse := c.BodyParser(&params)
+
+		if err_parse != nil {
+			log.Printf("Error parsing JSON: %v", err_parse)
+
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Invalid JSON",
+			})
+		}
+
+		err_validate := params.Validate()
+
+		if err_validate != nil {
+			log.Printf("Error parsing JSON: %v", err_parse)
+
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": err_validate.Error(),
+			})
+		}
+
+		var response = api.SolveVerityResponse{
+			OutsideDissectionSteps: []string{},
+			InsideDissectionSteps:  [][]string{},
+		}
+
+		return c.Status(fiber.StatusOK).JSON(response)
 	})
 }
